3ba33131077db5e6271b32a9c5c24e113ad5d9db
michael-r-perry <60802642+michael-r-perry@users.noreply.github.com>
2021-04-30 12:59:20 -0500

    Delete BusRoutes.py

--- BusRoutes.py
+++
@@ -1,7 +0,0 @@
-T = int(input())
-for t in range(1, T+1):
-    N, D = [int(num) for num in input().split(" ")]
-    bts = [int(num) for num in input().split(" ")][::-1]
-    for i in range(N):
-        D = D // bts[i] * bts[i]
-    print("Case #" + str(t) + ": " + str(D))
